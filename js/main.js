223394bbf0c819aecd4b49ad938718634e6bb2f2
terachan3700 <chapico01@gmail.com>
2018-12-19 22:44:25 +0900

    add api data get

--- js/main.js
+++ js/main.js
@@ -2,5 +2,11 @@ var app = new Vue({
   el: '#app',
   data: {
     bpi: null
+  },
+  mounted: function () {
+    axios.get("https://api.coindesk.com/v1/bpi/currentprice.json")
+    .then(function(response) {
+      console.log(response) 
+    })
   }
 })
