92e14ffd9cd0b35d1f5a7905079c4b432cc62497
terachan3700 <chapico01@gmail.com>
2018-12-19 23:20:56 +0900

    Add error handler

--- js/main.js
+++ js/main.js
@@ -1,12 +1,28 @@
 var app = new Vue({
   el: '#app',
   data: {
-    bpi: null
+    bpi: null,
+    hasError: false,
+    loading: true
   },
   mounted: function () {
     axios.get("https://api.coindesk.com/v1/bpi/currentprice.json")
     .then(function(response) {
-      console.log(response) 
-    })
+      // console.log(response.data.bpi) 
+      // console.log(response.data.bpi.USD.rate_float) 
+      this.bpi = response.data.bpi
+    }.bind(this))
+    .catch(function (error) {
+        console.log(error)
+        this.hasError = true
+    }.bind(this))
+    .finally(function () {
+      this.loading = false
+    }.bind(this))
+  },
+  filters: {
+    currencyDecimal(value) {
+      return value.toFixed(2)
+    }
   }
 })
